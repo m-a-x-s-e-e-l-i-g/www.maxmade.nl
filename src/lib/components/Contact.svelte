154e8781709274a3441a814638c0efb41ab92bff
Max Seelig <maxseelig@gmail.com>
2025-07-04 21:31:40 +0200

    feat: Add social media icons and update contact section with icon components

--- src/lib/components/Contact.svelte
+++ src/lib/components/Contact.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { LinkedInIcon, InstagramIcon, GitHubIcon } from './icons';
+	
 	let formData = $state({
 		email: '',
 		phone: '',
@@ -31,17 +33,20 @@
 		{
 			name: 'LinkedIn',
 			url: 'https://www.linkedin.com/in/maxse/',
-			icon: 'M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z'
+			description: 'Professional network',
+			iconComponent: LinkedInIcon
 		},
 		{
 			name: 'Instagram',
 			url: 'https://www.instagram.com/maxseelig_photography/',
-			icon: 'M12.017 0C5.396 0 .029 5.367.029 11.987c0 6.624 5.367 11.99 11.988 11.99s11.987-5.366 11.987-11.99C24.014 5.367 18.647.001 12.017.001zm6.624 18.021c-.415 1.563-1.205 2.353-2.768 2.768-1.344.377-4.543.292-6.044.292-1.501 0-4.7.085-6.044-.292-1.563-.415-2.353-1.205-2.768-2.768-.377-1.344-.292-4.543-.292-6.044 0-1.501-.085-4.7.292-6.044.415-1.563 1.205-2.353 2.768-2.768 1.344-.377 4.543-.292 6.044-.292 1.501 0 4.7-.085 6.044.292 1.563.415 2.353 1.205 2.768 2.768.377 1.344.292 4.543.292 6.044 0 1.501.085 4.7-.292 6.044z'
+			description: 'Photography portfolio',
+			iconComponent: InstagramIcon
 		},
 		{
 			name: 'GitHub',
 			url: 'https://github.com/m-a-x-s-e-e-l-i-g',
-			icon: 'M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z'
+			description: 'Code repositories',
+			iconComponent: GitHubIcon
 		}
 	];
 </script>
@@ -177,16 +182,12 @@
 								class="flex items-center text-gray-300 hover:text-white transition-colors group"
 							>
 								<div class="w-12 h-12 bg-gray-800 rounded-lg flex items-center justify-center mr-4 group-hover:bg-gray-700 transition-colors">
-									<svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20">
-										<path fill-rule="evenodd" d={social.icon} clip-rule="evenodd" />
-									</svg>
+									<social.iconComponent size={24} />
 								</div>
 								<div>
 									<div class="font-medium">{social.name}</div>
 									<div class="text-sm text-gray-400">
-										{social.name === 'LinkedIn' ? 'Professional network' : 
-										 social.name === 'Instagram' ? 'Photography portfolio' : 
-										 'Code repositories'}
+										{social.description}
 									</div>
 								</div>
 							</a>
---
+++ src/lib/components/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as LinkedInIcon } from './LinkedInIcon.svelte';
+export { default as InstagramIcon } from './InstagramIcon.svelte';
+export { default as GitHubIcon } from './GitHubIcon.svelte';
+export { default as EmailIcon } from './EmailIcon.svelte';
