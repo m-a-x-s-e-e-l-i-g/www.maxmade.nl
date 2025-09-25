4e0407cd5399095a2981cfdcb303ca38580ae950
Max Seelig <maxseelig@gmail.com>
2025-09-25 19:49:11 +0200

    .

--- src/routes/projects/jumpflix-tv/+page.svelte
+++ src/routes/projects/jumpflix-tv/+page.svelte
@@ -126,7 +126,7 @@
 								<svg class="mr-2 h-4 w-4 text-green-500" fill="currentColor" viewBox="0 0 20 20">
 									<path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
 								</svg>
-								MIT License
+								CC BY-NC-ND 4.0
 							</div>
 						</div>
 					</div>
