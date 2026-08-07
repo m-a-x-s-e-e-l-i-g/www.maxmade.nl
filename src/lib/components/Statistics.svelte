66d79f434445e4394a60db579d3648a5563c4764
Max Seelig <7907436+m-a-x-s-e-e-l-i-g@users.noreply.github.com>
2026-08-07 06:00:50 +0200

    chore: update daily statistics [2026-08-07]

--- src/lib/components/Statistics.svelte
+++ src/lib/components/Statistics.svelte
@@ -10,8 +10,8 @@
 			metrics: [
 				{ label: 'Total Contributions', value: '5,284' },
 				{ label: 'Total Stars Earned', value: '108' },
-				{ label: 'Commits (2026)', value: '665' },
-				{ label: 'Pull Requests', value: '135' }
+				{ label: 'Commits (2026)', value: '686' },
+				{ label: 'Pull Requests', value: '141' }
 			],
 			color: 'from-gray-600 to-gray-800',
 			link: 'https://github.com/m-a-x-s-e-e-l-i-g'
@@ -32,9 +32,9 @@
 			icon: '📺',
 			metrics: [
 				// Updated 2025-09-29 from profile page (All Time watched)
-				{ label: 'Shows', value: '150d 10h 29m' },
+				{ label: 'Shows', value: '150d 10h 30m' },
 				{ label: 'Episodes', value: '5,806' },
-				{ label: 'Movies', value: '146d 17h 15m' },
+				{ label: 'Movies', value: '146d 17h 11m' },
 				{ label: 'Films', value: '1,767' }
 			],
 			color: 'from-red-600 to-red-800',
