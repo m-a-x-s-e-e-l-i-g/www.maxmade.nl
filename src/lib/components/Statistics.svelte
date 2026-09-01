fcd510de8f0a83b91479510b1ceae976a928db13
Max Seelig <7907436+m-a-x-s-e-e-l-i-g@users.noreply.github.com>
2026-09-01 06:00:48 +0200

    chore: update daily statistics [2026-09-01]

--- src/lib/components/Statistics.svelte
+++ src/lib/components/Statistics.svelte
@@ -10,7 +10,7 @@
 			metrics: [
 				{ label: 'Total Contributions', value: '5,284' },
 				{ label: 'Total Stars Earned', value: '113' },
-				{ label: 'Commits (2026)', value: '751' },
+				{ label: 'Commits (2026)', value: '752' },
 				{ label: 'Pull Requests', value: '147' }
 			],
 			color: 'from-gray-600 to-gray-800',
@@ -32,10 +32,10 @@
 			icon: '📺',
 			metrics: [
 				// Updated 2025-09-29 from profile page (All Time watched)
-				{ label: 'Shows', value: '150d 18h 15m' },
-				{ label: 'Episodes', value: '5,816' },
-				{ label: 'Movies', value: '147d 6h 3m' },
-				{ label: 'Films', value: '1,776' }
+				{ label: 'Shows', value: '150d 19h 48m' },
+				{ label: 'Episodes', value: '5,817' },
+				{ label: 'Movies', value: '147d 7h 32m' },
+				{ label: 'Films', value: '1,777' }
 			],
 			color: 'from-red-600 to-red-800',
 			link: 'https://trakt.tv/users/garlfed'
