076d0bf19db173b8c5b2b12aa71e9c6e7b18e834
Max Seelig <7907436+m-a-x-s-e-e-l-i-g@users.noreply.github.com>
2026-09-07 06:00:46 +0200

    chore: update daily statistics [2026-09-07]

--- src/lib/components/Statistics.svelte
+++ src/lib/components/Statistics.svelte
@@ -10,7 +10,7 @@
 			metrics: [
 				{ label: 'Total Contributions', value: '5,284' },
 				{ label: 'Total Stars Earned', value: '113' },
-				{ label: 'Commits (2026)', value: '779' },
+				{ label: 'Commits (2026)', value: '780' },
 				{ label: 'Pull Requests', value: '147' }
 			],
 			color: 'from-gray-600 to-gray-800',
@@ -22,7 +22,7 @@
 			metrics: [
 				// Updated 2026-02-13 via xboxgamertag.com
 				{ label: 'Gamerscore', value: '11,575' },
-				{ label: 'Games Played', value: '0' }
+				{ label: 'Games Played', value: '30' }
 			],
 			color: 'from-green-600 to-green-800',
 			link: 'https://xboxgamertag.com/search/Snurkpop'
@@ -32,8 +32,8 @@
 			icon: '📺',
 			metrics: [
 				// Updated 2025-09-29 from profile page (All Time watched)
-				{ label: 'Shows', value: '150d 20h 50m' },
-				{ label: 'Episodes', value: '5,818' },
+				{ label: 'Shows', value: '150d 21h 50m' },
+				{ label: 'Episodes', value: '5,819' },
 				{ label: 'Movies', value: '147d 14h 1m' },
 				{ label: 'Films', value: '1,778' }
 			],
